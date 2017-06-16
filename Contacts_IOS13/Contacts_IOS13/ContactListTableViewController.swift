1e8717c28bda48aba771f4177478cdac11033168
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-16 10:23:00 -0600

    Implement ContactListTableViewController

--- Contacts_IOS13/Contacts_IOS13/ContactListTableViewController.swift
+++ Contacts_IOS13/Contacts_IOS13/ContactListTableViewController.swift
@@ -7,72 +7,76 @@
 //
 
 import UIKit
+import CloudKit
 
 class ContactListTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(refresh), name: ContactController.contactsUpdatedNotification, object: nil)
+        
+        // Initial Fetch
+        ContactController.fetchNewContacts()
     }
-
-    // MARK: - Table view data source
-
+    
+    func refresh() {
+        tableView.reloadData()
+    }
+    
+    //MARK: - Table view data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return ContactController.contacts.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let contact = ContactController.contacts[indexPath.row]
+        
+        cell.textLabel?.text = contact.name
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let contact = ContactController.contacts[indexPath.row]
+            let recordID = CKRecordID.init(recordName: contact.recordID)
+            
+            CloudKitManager.deleteRecords(RecordID: recordID, completion: { (_, error) in
+                
+                if let error = error {
+                    NSLog("Error deleting data \(error.localizedDescription)")
+                    return
+                }
+            })
+            
+            ContactController.contacts.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "editContactSegue" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let detailViewController = segue.destination as? ContactDetailViewController else {
+                    NSLog("editContactSegue Error")
+                    return
+            }
+            
+            let contact = ContactController.contacts[indexPath.row]
+            
+            //FIXME: - This is what I need to set on my detailView when I make it.
+//            detailViewController.contact = contact
+//            detailViewController.isEditingContact = true
+//            detailViewController.index = indexPath.row
+        }
     }
-    */
-
 }
